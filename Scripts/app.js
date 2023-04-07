8589b36dfc17bfc7a1fde42e857983b729c2d112
Anila_Jamil <Anila_Jamil@banoqabil.com>
2023-04-08 01:13:33 +0500

    FEAT:Zakat Calculator

---
+++ Scripts/app.js
@@ -0,0 +1,5 @@
+var userInput =+ prompt("Your amount please");
+var zakatPercentage =("0.025");
+console.log=(zakatPercentage)
+var result=(Number(userInput) * Number(zakatPercentage))
+alert('Your Zakat is '+ result)
