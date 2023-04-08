e7d4920d3d5d167e577d9f99a4071530e88d59aa
Anila_Jamil <Anila_Jamil@banoqabil.com>
2023-04-09 03:06:02 +0500

    Practice js file make

---
+++ Scripts/practice.js
@@ -0,0 +1,101 @@
+  //DAY=01   
+     
+     //alert
+
+//alert ("this is first day of javascript");
+//alert ('can be written in single quotes');
+//number values can be written without ' or ""
+//alert(5+4);     //it will add up the numbers alert can be use for calculation as well
+
+      //string (for_variables) and prompt
+/*var user=prompt('your name please','anila') 
+alert(user);*/
+
+/*var arr=['hello','hi']
+document.write(arr)
+console.log;(arr)*/
+
+/*var anilaJamilWebDev="cases of stings,1st word small other can be capital";  //camelCase
+var AnilaJamilWebDev="cases of stings,1st word can be capital";  //PascalCase
+var anila_Jamil_Web_Dev="cases of stings,with underscore for space";  //snake_Case*/
+
+/*var name = "Mark";
+console.log(name);*/
+
+/*var nationality; 
+nationality = "U.S.";
+console.log(nationality);*/
+
+//alert("Thanks for your input!");
+
+/*var thanx = "Thanks for your input!" 
+alert(thanx);*/
+
+/*var num=143
+console.log(num)*/
+
+/*var weight = 150;
+console.log(weight);*/
+
+/*var age = 27; 
+ alert(age);*/
+
+/* var my_name = 'Anila';
+ alert(my_name);*/
+
+/* var first_name = 'Anila';
+ var last_name = 'Jamil';
+ alert(first_name + last_name);*/
+
+/* var first_name = 'Anila';
+ var last_name = 'Jamil';
+ alert(first_name +" "+ last_name);   //to add spacae b/w two names we add " "(inverted comas and space b/w the 2)*/
+
+/* var age = 18;
+ alert('Your age is ' + age); //signle quotes and space also adds space b/w nums and variables*/
+
+/*var email='yourname@gmail.com';
+alert(email)*/
+
+/*var num1 =+ prompt("Num 1 Please");
+var num2 =+ prompt("Num 2 Please");
+console.log(Number(num1) + Number(num2));*/   //this condition will also add the numbers
+
+/*var num1=+ prompt("Num 1 Please");
+var num2=+ prompt("Num 2 Please");
+console.log(num1+num2);*/         //if the numbers aren't in comas then they will be added
+
+/*var num1=+ prompt("number 1 please")
+var num2=+ prompt("number 2 please")
+console.log("2"+"3")*/            //inverted comas will combine the numbers it won't add them
+//console.log(20+5)   //numbers would be added answer=25
+//console.log("20"+"5")   //numbers would only combine
+//console.log(20+5+'5')   //it will add starting two numbers and the third one would only combine as it's written in comas
+//console.log(20+'10')   //this condition will also combine the numbers answer would be 2010
+
+//console.log(2+8)
+//console.log(2-8)
+//console.log(2*8)
+//console.log(2**8)  //for power(times) //in simple words we can double the numbers and multipy them**= 8*2*2*8=256
+//console.log(2/8)
+
+      //expressions would solve via BODMAS rule
+//console.log(2 + 5 - 6 * 3 / 9);          //1)3/9=0.33,2)6*0.33=2,3)2+5=7,4)sub the ans of mul i.e 7-2  ans=5
+//console.log((2 + 5) - 6 * 3 / 9);          // bracketes added to be more specic with the calculation now brackets will be solved first ans=5
+//console.log(2 + (5 - 6) * 3 / 9)            //ans=1.666
+
+
+
+// console.log(((1+(6/7))*(8/9)+6)-(12*5)/3)
+
+// var name = "Anila"
+// var fname = "Jamil"
+// console.log(name+ " " + fname)
+
+// var num = 5
+// console.log(num)
+// num = num+20
+// num *=20
+// num++
+// ++num
+// console.log(num)
